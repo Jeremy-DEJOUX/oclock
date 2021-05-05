8cd91c2536dea96887e347f79b14fd8bd471b49d
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-05-05 13:40:14 +0200

    ajout icon awesome

--- index.php
+++ index.php
@@ -6,15 +6,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/56188ecd90.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
     <title>O'clockt</title>
+    
 </head>
     <body>
         <header></header>
 
         <main>
             <h1>O'Clock</h1>
+
+            <div>
+                <h2>Minuteur</h2>
+                <i class="far fa-hourglass"></i>
+            </div>
+
+            <div>
+                <h2>Chronomètre</h2>
+                <i class="fas fa-stopwatch"></i>
+            </div>
+
+            <div>
+                <h2>Réveil</h2>
+                <i class="fas fa-bell"></i>
+            </div>
+
+            <div>
+                <h2>Horloge</h2>
+                <i class="fas fa-clock"></i>
+            </div>
         </main>
 
         <footer></footer>
+        <script src="ressources/script.js"></script>
     </body>
 </html>
