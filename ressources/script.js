56dd8fc30f736ffd17556a049b5203b86d058ab2
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-05-12 12:34:25 +0200

    Minuteur en cours reste plus & moins

--- ressources/script.js
+++ ressources/script.js
@@ -189,5 +189,29 @@ startstop.onclick = function(){
 
 // MINUTEUR======================
 
+let startMinuteur = document.getElementById('startMinuteur');
 
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+startMinuteur.onclick = function () {
+    var fiveMinutes = (document.getElementById('time').value * 60);
+    console.log(document.getElementById('timer').value)
+    display = document.querySelector('#timeDisplay');
+    startTimer(fiveMinutes, display);
+};
